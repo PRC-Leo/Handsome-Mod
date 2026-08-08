6195f583685128fb9200f557a0ff8a1b288ca543
PRC-Leo <3778385145@qq.com>
2026-08-08 10:19:23 +0800

    构建

---
+++ src/main/java/handsome/HandsomeModClient.java
@@ -0,0 +1,10 @@
+package handsome;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HandsomeModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
